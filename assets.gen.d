2145de90096f0ba1ba629ee8e49b2246b2c3a0b4
cpottamus <cp3192@gmail.com>
2014-04-21 12:01:06 -0500

    first commit

---
+++ assets.gen.d
@@ -0,0 +1,55 @@
+assets.gen.o: assets.gen.cpp \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/group.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/syscall.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/types.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/audio.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/events.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/vram.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/asset.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/elf.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/limits.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/macros.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/math.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/loader.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/array.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/memory.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/audio.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/image.h
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/group.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/syscall.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/types.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/audio.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/events.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/vram.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/asset.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/elf.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/limits.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/macros.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/math.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/loader.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/array.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/memory.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/audio.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/image.h:
---
+++ main.d
@@ -0,0 +1,115 @@
+main.o: main.cpp \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/syscall.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/types.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/audio.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/events.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/vram.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/asset.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/elf.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/array.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/memory.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/macros.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/group.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/limits.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/math.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/loader.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/audio.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/image.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/audio.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/cube.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/event.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/filesystem.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/metadata.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/motion.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/string.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/system.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/time.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/color.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/sprite.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/framebuffer.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/bg0rom.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/bg0.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/bg1.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/bg2.h \
+  /Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/tilebuffer.h \
+  assets.gen.h
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/syscall.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/types.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/audio.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/events.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/vram.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/asset.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/elf.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/array.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/memory.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/macros.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/group.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/limits.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/math.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/loader.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/audio.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/image.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/audio.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/cube.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/event.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/filesystem.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/metadata.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/motion.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/string.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/system.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/time.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/color.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/sprite.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/framebuffer.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/bg0rom.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/bg0.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/bg1.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/bg2.h:
+
+/Users/carsonpotter/Desktop/School/Tangibles/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/tilebuffer.h:
+
+assets.gen.h:
